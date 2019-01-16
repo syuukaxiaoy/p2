52620918f291c17e6096bc2a9071c9c165203b17
ZHOU HUA <1915671841@qq.com>
2019-01-16 17:42:47 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,14 +1,13 @@
+import torch
 import argparse
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import torch
-
 from torch import nn
 from torch import optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 from PIL import Image
+import matplotlib.pyplot as plt
 import json
 import sys
 
